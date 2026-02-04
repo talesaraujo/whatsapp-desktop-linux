54bb96c5242d687c9fc6cf433b7de7e07ced2c9b
Tales Araujo <talesaraujodossantos@gmail.com>
2026-02-04 14:42:36 -0300

    Fixed svg icon size

--- src/preload.ts
+++ src/preload.ts
@@ -1,19 +1,21 @@
 import { ipcRenderer } from "electron";
 
 function overrideNotification() {
-    window.Notification = class extends Notification {
-        constructor(title: string, options: NotificationOptions) {
-            super(title, options);
-            this.onclick = _event => ipcRenderer.send("notification-click");
-        }
+  window.Notification = class extends Notification {
+    constructor(title: string, options: NotificationOptions) {
+      super(title, options);
+      this.onclick = (_event) => ipcRenderer.send("notification-click");
     }
+  };
 }
 
 function handleChromeVersionBug() {
-    window.addEventListener("DOMContentLoaded", () => {
-        if (document.getElementsByClassName("landing-title version-title").length != 0)
-            ipcRenderer.send("chrome-version-bug");
-    });
+  window.addEventListener("DOMContentLoaded", () => {
+    if (
+      document.getElementsByClassName("landing-title version-title").length != 0
+    )
+      ipcRenderer.send("chrome-version-bug");
+  });
 }
 
 overrideNotification();
